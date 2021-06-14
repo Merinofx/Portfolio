bd9aaef4470581b7a35daa531babbedd05de3eda
Alex Eng <Alexeng7@gmail.com>
2021-06-14 11:11:04 -0400

    Angular of Heroes Tour

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Alex' },
+    { id: 12, name: 'Caleb' },
+    { id: 13, name: 'Andrew' },
+    { id: 14, name: 'Sunny' },
+    { id: 15, name: 'Eleonor' },
+    { id: 16, name: 'Teemo' },
+    { id: 17, name: 'Jett' },
+    { id: 18, name: 'Sage' },
+    { id: 19, name: 'Breach' },
+    { id: 20, name: 'Omen' }
+];
